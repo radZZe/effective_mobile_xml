30202052e65f0bcf9a4f1c09017dd73578cdfa67
radZZe <mimozadr@gmail.com>
2024-02-03 21:59:10 +1000

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "effective_mobile_xml"
+include(":app")
+include(":features:auth")
+include(":features:catalog")
+include(":features:profile")
+include(":features:home")
+include(":data")
